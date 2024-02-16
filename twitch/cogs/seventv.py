848e2af0753cdf55021696997b367a5e9d2b79bc
Kazzuun <pyry.kielosto@gmail.com>
2024-02-16 22:17:20 +0200

    Add topemotes command

--- twitch/cogs/seventv.py
+++ twitch/cogs/seventv.py
@@ -58,6 +58,18 @@ class SevenTV(commands.Cog):
 
 
     @commands.cooldown(rate=1, per=COG_COOLDOWN, bucket=commands.Bucket.member)
+    @commands.command()
+    async def topemotes(self, ctx: commands.Context):
+        """Shows top 10 emotes from the past 1000 messages"""
+        emotes = await seventv.channel_emotes(ctx.channel_id)
+        global_emotes = await seventv.global_emotes()
+        emotes.extend(global_emotes)
+        emote_count = await database.emote_counts(ctx.channel.name, [emote["name"] for emote in emotes])
+        top_10 = emote_count.most_common(10)
+        await self.bot.message_queues.queue_command(ctx, " | ".join([f"{i + 1}. {emote[0]} - {emote[1]}" for i, emote in enumerate(top_10)]))
+
+
+    @commands.cooldown(rate=1, per=COG_COOLDOWN, bucket=commands.Bucket.member)
     @commands.command(aliases=("randomemote", "randemote"))
     async def re(self, ctx: commands.Context, count: int):
         """Sends random emotes(s) from the channel; {prefix}re <count>"""
