854026b458297ee214b628dba82e0e15799b7cac
Kazzuun <pyry.kielosto@gmail.com>
2024-02-17 01:17:08 +0200

    Add a possibility to set a reminder to fish when fishing; fix most common emote not being most common fish and giving the least exp

--- twitch/cogs/fish.py
+++ twitch/cogs/fish.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 from typing import Optional
 
@@ -23,8 +23,11 @@ class Fish(commands.Cog):
 
     @commands.cooldown(rate=1, per=1*60*60, bucket=commands.Bucket.user)
     @commands.command(aliases=("fishinge", "🐟"))
-    async def fish(self, ctx: commands.Context):
-        """You go fishing; number of fish you catch is based on luck, fishing level, and time since last fished"""
+    async def fish(self, ctx: commands.Context, *args):
+        """
+        You go fishing; number of fish you catch is based on luck, fishing level, and time since last fished;
+        set a reminder to remind you to fish in 1 hour with -r flag
+        """
         current_time = datetime.utcnow()
         last_fished = await database.last_fished(ctx.author.id)
         seconds_ellapsed = (current_time - last_fished).seconds if last_fished else 1*60*60
@@ -48,20 +51,37 @@ class Fish(commands.Cog):
         channel_emotes.extend(global_emotes)
 
         most_used_emotes = await database.emote_counts(ctx.channel.name, channel_emotes)
-        emotes_ordered = most_used_emotes.most_common(10)[::-1]
+        top_n = 10
+        emotes_ordered = most_used_emotes.most_common(top_n)
 
         fish_caught = random.choices(
-            [(emote[0], i + 1) for i, emote in enumerate(emotes_ordered)], 
-            cum_weights=[(j + 1) ** 0.6 for j in range(len(emotes_ordered))], 
+            # Give the most common the highest index
+            [(emote[0], top_n - i) for i, emote in enumerate(emotes_ordered)],
+            # The most common has the highest weight
+            weights=[1 / (j + 1) for j in range(len(emotes_ordered))], 
             k=total_fish_count
         )
-        total_exp_amount = int(sum([1 / index * 30 for _, index in fish_caught]))
+        # The most common has the highest index getting the least exp
+        total_exp_amount = int(sum([60 / index for _, index in fish_caught]))
 
         exp_after = exp_before + total_exp_amount
         level_after = self.level_from_exp(exp_after)
         level_up_notif = "" if level_before == level_after else f", LEVEL UP {level_before}->{level_after}!"
-        
+
         await database.fish(ctx.author.id, ctx.author.name, total_fish_count, total_exp_amount)
+
+        if "-r" in args:
+            rem_id = await database.set_reminder(
+                ctx.channel.name, 
+                ctx.author.name, 
+                ctx.author.name, 
+                database.ReminderType.REMIND,
+                f"{self.bot.prefixes[0]}fish",
+                datetime.utcnow() + timedelta(hours=1)
+            )
+            reminder_msg = f" | Reminding you to fish in 1 hour (ID {rem_id})"
+        else:
+            reminder_msg = ""
 
         fish_caught_count = {}
         for name, _ in fish_caught:
@@ -70,7 +90,7 @@ class Fish(commands.Cog):
         caught = " | ".join([f"{name} - {count}" for name, count in fish_caught_count.items()])
         await self.bot.message_queues.queue_command(
             ctx, 
-            f"You caught {total_fish_count} fish: {caught}, exp got: {total_exp_amount}{level_up_notif}", 
+            f"You caught {total_fish_count} fish: {caught}, exp got: {total_exp_amount}{level_up_notif}{reminder_msg}", 
             reply=True
         )
 
